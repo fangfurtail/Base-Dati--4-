36044c9b68c8f08ed1725193ee4d1528909c0d76
Kaan Bilgili <153911605+fangfurtail@users.noreply.github.com>
2025-05-17 12:58:31 +0200

    CRUD operations

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ from models import Student
 from datetime import date
 from database import SessionLocal , engine, Base
 
-Base.metadata.create_all(bind=engine)
 session = SessionLocal()
 
 def list_students():
@@ -10,4 +9,68 @@ def list_students():
     for student in students:
         print(f"{student.StudentID}: {student.FirstName} {student.LastName}, DOB: {student.DateOfBirth}, Address: {student.Address}")
 
-list_students()
+def add_student():
+    sid = int(input("Student ID: "))
+    fname = input("First Name: ")
+    lname = input("Last Name: ")
+    dob = input("Date of Birth (YYYY-MM-DD): ")
+    address = input("Address: ")
+
+    new_student = Student(
+    StudentID=sid,
+    FirstName=fname,
+    LastName=lname,
+    DateOfBirth=date.fromisoformat(dob),
+    Address=address
+    )
+    session.add(new_student)
+    session.commit()
+    print("Student added successfully!")
+
+
+def delete_student():
+    sid = int(input("Enter Student ID to delete: "))
+    student= session.query(Student).where(Student.StudentID == sid).first()
+
+    if student:
+        session.delete(student)
+        session.commit()
+        print("Student deleted successfully!")
+    else:
+        print("Student not found.")
+
+def update_student():
+    sid = int(input("Enter Student ID to update: ")) 
+    student = session.query(Student).filter(Student.StudentID == sid).first()  
+
+    if student:
+       
+        print(f"Current Info - Name: {student.FirstName} {student.LastName}, DOB: {student.DateOfBirth}, Address: {student.Address}")
+
+        fname = input("New First Name (leave blank to keep current): ")
+        lname = input("New Last Name (leave blank to keep current): ")
+        dob = input("New Date of Birth (YYYY-MM-DD) (leave blank to keep current): ")
+        address = input("New Address (leave blank to keep current): ")
+
+        if fname:
+            student.FirstName = fname
+        if lname:
+            student.LastName = lname
+        if dob:
+            try:
+                student.DateOfBirth = date.fromisoformat(dob)
+            except ValueError:
+                print("Invalid date format! Date not updated.")
+        if address:
+            student.Address = address
+
+        session.commit()  
+        print("Student updated successfully!")
+    else:
+        print("Student not found.")  
+
+
+
+
+
+main()
